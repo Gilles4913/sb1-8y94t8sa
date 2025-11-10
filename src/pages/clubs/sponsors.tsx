2ab64904c4a0902a4d00ebf37c9656cd13b18031
Gilles4913 <gilles.guerrin@a2display.fr>
2025-11-10 17:49:37 +0100

    Update sponsors.tsx

--- src/pages/clubs/sponsors.tsx
+++ src/pages/clubs/sponsors.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
-import { createClient } from '@supabase/supabase-js'
+import supabase from '@/lib/supabase'
 import { useTenant } from '@/contexts/TenantContext'
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!
-)
 
 type Sponsor = {
   id: string
