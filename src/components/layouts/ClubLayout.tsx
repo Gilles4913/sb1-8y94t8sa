6f2f9e14a256a2c77266f29f2c67ed29d7415f79
Gilles4913 <gilles.guerrin@a2display.fr>
2025-11-10 15:51:55 +0100

    Update ClubLayout.tsx

--- src/components/layouts/ClubLayout.tsx
+++ src/components/layouts/ClubLayout.tsx
@@ -1,14 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import TopNav from '@/components/layouts/TopNav'
-import ImpersonationBar from '@/components/admin/ImpersonationBar'
-import ClubSubnav from '@/components/layouts/ClubSubnav'
 
 export default function ClubLayout() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900 dark:bg-zinc-950 dark:text-gray-100">
       <TopNav />
-      <ImpersonationBar />
-      <ClubSubnav />
       <main className="mx-auto w-full max-w-6xl p-4">
         <Outlet />
       </main>
