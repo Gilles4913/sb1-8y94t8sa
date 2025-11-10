fefc138a4f30862962bff8b136b5095412260797
Gilles4913 <gilles.guerrin@a2display.fr>
2025-11-10 15:51:32 +0100

    Update AdminLayout.tsx

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import TopNav from '@/components/layouts/TopNav'
-import ImpersonationBar from '@/components/admin/ImpersonationBar'
 
 export default function AdminLayout() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900 dark:bg-zinc-950 dark:text-gray-100">
       <TopNav />
-      <ImpersonationBar />
       <main className="mx-auto w-full max-w-6xl p-4">
         <Outlet />
       </main>
