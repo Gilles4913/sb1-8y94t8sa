577a03ff7f95e6c396eebd9f16ac11c0a47781bf
Gilles4913 <gilles.guerrin@a2display.fr>
2025-11-10 10:51:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/router'
 import { TenantProvider } from '@/contexts/TenantContext'
+import { ThemeProvider } from '@/contexts/ThemeContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TenantProvider>
-      <RouterProvider router={router} />
-    </TenantProvider>
+    <ThemeProvider>
+      <TenantProvider>
+        <RouterProvider router={router} />
+      </TenantProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
