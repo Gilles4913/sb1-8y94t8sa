b98b3f423b73d887d57114166b659d4e305558ee
Gilles4913 <gilles.guerrin@a2display.fr>
2025-11-10 10:12:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config
